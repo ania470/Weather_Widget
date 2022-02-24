5c9283fa2278d9301b0b407673a323118d1c141d
Anna Jarosz <aniauchman92@gmail.com>
2022-02-24 12:35:33 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    background: red;
+`;
+
+function App() {
+    return (
+        <Wrapper>
+            <p>test</p>
+        </Wrapper>
+    );
+}
+
+export default App;
